26f3c1cbd8b9d4b863c7fa9ebfddc30e14a007f5
Kamil Surma <kamil.surma@wp.pl>
2018-12-12 10:39:41 +0100

    Dodanie kolejnego testu

--- szkolenie/src/test/java/selenium/basic/SeleniumTest.java
+++ szkolenie/src/test/java/selenium/basic/SeleniumTest.java
@@ -22,25 +22,57 @@ public class SeleniumTest {
 
     @BeforeEach
     public void startDriver() {
+        System.setProperty("webdriver.chrome.driver","c:\\drivers\\chrome\\chromedriver.exe");
        driver = new ChromeDriver();
     }
 
 
     @Test
     public void canFindCodeSprintersOnGoogle() {
-   System.setProperty("webdriver.chrome.driver","c:\\drivers\\chrome\\chromedriver.exe");
+
         driver.get("https://google.com");
       WebElement searchBox = driver.findElement(By.name("q"));
       searchBox.sendKeys("code sprinters");
       searchBox.submit();
         Stream<WebElement> searchResults = driver.findElements(By.cssSelector("div.rc")).stream();
+
         Stream<WebElement> resultFiltered  = searchResults.filter(r -> r.findElement(By.cssSelector("div.r > a")).getAttribute("href").equals("http://agileszkolenia.pl/"));
         List<WebElement> resultList  = resultFiltered.collect(Collectors.toList());
+
         Assertions.assertEquals(1 , resultList.size());
         WebElement result  = resultList.get(0);
-        Assertions.assertEquals("agileszkolenia.pl/" , driver.findElement(By.tagName("cite")).getText());
+        Assertions.assertEquals("agileszkolenia.pl/" , result.findElement(By.tagName("cite")).getText());
 
     }
+
+    @Test
+    public void  verifyFindAuthorOfBlogNoteAboutTransformtion () {
+        driver.get("http://markowicz.pro/");
+        Stream<WebElement> listOfNotes = driver. findElements(By.className("entry-title"))
+                .stream()
+                .filter(n -> n.findElement(By.tagName("a")).getText().equals("O transformacjach"));
+        List<WebElement> filteredNotes  = listOfNotes.collect(Collectors.toList());
+        Assertions.assertEquals(1 , filteredNotes.size() , "Only one matching note is found on main page");
+    }
+
+    @Test
+
+    public void verifyAuthorOfBlogNoteAboutTransformationsStepByStep() {
+
+        driver.get("http://markowicz.pro/");
+
+        List<WebElement> listOfNotes = driver.findElements(By.className("entry-title"));
+        Stream<WebElement> streamOfNotes = listOfNotes.stream();
+        Stream<WebElement> filteredStream = streamOfNotes
+        .filter(n -> n.findElement(By.tagName("a")).getText().equals("O transformacjach"));
+        List<WebElement> filteredNotes = filteredStream.collect(Collectors.toList());
+        Assertions.assertEquals(1, filteredNotes.size(), "Only one matching note is found on main page");
+
+
+
+    }
+
+
 
     @AfterEach
     public void closeDriver() {
